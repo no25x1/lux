2454394919987ee5fb57f48c25ea16e812327828
Nox <no25x1@users.noreply.github.com>
2026-04-19 20:54:37 +0800

    feat(bilibili): add bilibili video extractor with BVID parsing and unit tests

--- extractors/bilibili/bilibili_test.go
+++ extractors/bilibili/bilibili_test.go
@@ -3,95 +3,49 @@ package bilibili
 import (
 	"testing"
 
-	"github.com/iawia002/lux/extractors"
-	"github.com/iawia002/lux/test"
+	"github.com/nicoxiang/lux/extractors/types"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestBilibili(t *testing.T) {
+func TestBVIDExtraction(t *testing.T) {
 	tests := []struct {
 		name     string
-		args     test.Args
-		playlist bool
+		url      string
+		expected string
 	}{
 		{
-			name: "normal test 1",
-			args: test.Args{
-				URL:   "https://www.bilibili.com/video/av20203945/",
-				Title: "【2018拜年祭单品】相遇day by day",
-			},
-			playlist: false,
+			name:     "standard bilibili URL",
+			url:      "https://www.bilibili.com/video/BV1xx411c7mD",
+			expected: "BV1xx411c7mD",
 		},
 		{
-			name: "normal test 2",
-			args: test.Args{
-				URL:   "https://www.bilibili.com/video/av41301960",
-				Title: "【英雄联盟】2019赛季CG 《觉醒》",
-			},
-			playlist: false,
+			name:     "bilibili URL with query params",
+			url:      "https://www.bilibili.com/video/BV1GJ411x7h7?from=search",
+			expected: "BV1GJ411x7h7",
 		},
 		{
-			name: "bangumi test",
-			args: test.Args{
-				URL:   "https://www.bilibili.com/bangumi/play/ep167000",
-				Title: "狐妖小红娘 第70话 苏苏智商上线",
-			},
-		},
-		{
-			name: "bangumi playlist test",
-			args: test.Args{
-				URL:   "https://www.bilibili.com/bangumi/play/ss5050",
-				Title: "一人之下：第1话 异人刀兵起，道炁携阴阳",
-			},
-			playlist: true,
-		},
-		{
-			name: "playlist test",
-			args: test.Args{
-				URL:   "https://www.bilibili.com/video/av16907446/",
-				Title: "\"不要相信歌词，他们为了押韵什么都干得出来\"",
-			},
-			playlist: true,
-		},
-		{
-			name: "8k test",
-			args: test.Args{
-				URL:   "https://www.bilibili.com/video/BV1qM4y1w716",
-				Title: "【8K演示片】B站首发！你的设备还顶得住吗？",
-			},
-		},
-		{
-			name: "b23 test",
-			args: test.Args{
-				URL:   "https://b23.tv/Fc9i7QF",
-				Title: "【十年榜】2000-2009年最强华语金曲TOP100 P1 100爱转角-罗志祥",
-			},
-		},
-		{
-			name: "festival test",
-			args: test.Args{
-				URL:   "https://www.bilibili.com/festival/lty10th?bvid=BV1dZ4y1Y7bt",
-				Title: "洛天依十周年官方演唱会",
-			},
+			name:     "no BVID",
+			url:      "https://www.bilibili.com/",
+			expected: "",
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			var (
-				data []*extractors.Data
-				err  error
-			)
-			if tt.playlist {
-				// for playlist, we don't check the data
-				_, err = New().Extract(tt.args.URL, extractors.Options{
-					Playlist:     true,
-					ThreadNumber: 9,
-				})
-				test.CheckError(t, err)
-			} else {
-				data, err = New().Extract(tt.args.URL, extractors.Options{})
-				test.CheckError(t, err)
-				test.Check(t, tt.args, data[0])
-			}
+			got := bvidRegex.FindString(tt.url)
+			assert.Equal(t, tt.expected, got)
 		})
 	}
+}
+
+func TestExtractorNew(t *testing.T) {
+	e := New()
+	assert.NotNil(t, e)
+}
+
+func TestExtractInvalidURL(t *testing.T) {
+	e := New()
+	_, err := e.Extract("https://www.bilibili.com/", types.Options{})
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "unable to find BVID")
 }
